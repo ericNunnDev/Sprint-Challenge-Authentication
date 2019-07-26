d49c89e9d2edd6a10714d417d5970e3494813a7e
lowlyHero <e.nunn1009@gmail.com>
2019-07-26 12:54:29 -0400

    working on login and registration, .env added

--- auth/authenticate.js
+++ auth/authenticate.js
@@ -1,24 +1,18 @@
 const jwt = require('jsonwebtoken');
 
-const jwtKey =
-  process.env.JWT_SECRET ||
-  'add a .env file to root of project with the JWT_SECRET variable';
+const jwtKey = process.env.JWT_SECRET || 'Whats in the secret box?';
 
-// quickly see what this file exports
 module.exports = {
   authenticate,
 };
 
-// implementation details
 function authenticate(req, res, next) {
   const token = req.get('Authorization');
 
   if (token) {
     jwt.verify(token, jwtKey, (err, decoded) => {
       if (err) return res.status(401).json(err);
-
       req.decoded = decoded;
-
       next();
     });
   } else {
--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,7 @@
+const router = require('express').Router();
 const axios = require('axios');
-
+const bcrypt = require('bcryptjs');
+const db = require('./model');
 const { authenticate } = require('../auth/authenticate');
 
 module.exports = server => {
@@ -8,12 +10,35 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
+router.get('/jokes', authenticate, (req, res) => {
+
+});
+
 function register(req, res) {
-  // implement user registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  db.add(user)
+  .then(saved => {
+    res.status(200).json(saved);
+  })
+  .catch(e => res.status(500).json(e));
 }
 
-function login(req, res) {
-  // implement user login
+function login (req, res) {
+  let { username, password} = req.body;
+
+  db.findBy({ username })
+  .first()
+  .then(user => {
+    if(user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({ message: `Welcome ${user.username}!` });
+    } else {
+      res.status(401).json({ message: 'Invalid username or password.' })
+    }
+  })
+  .catch(e => res.status(500).json(e));
 }
 
 function getJokes(req, res) {
